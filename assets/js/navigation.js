431c8468fd431b80e74920b74d6ad567b9661f4c
pattlebass <49322676+pattlebass@users.noreply.github.com>
2023-04-14 22:22:05 +0300

    Improve accessibility for the navigation menu

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,9 +1,12 @@
 const navbarLinks = document.querySelector(".navbar-links");
+const toggleButton = document.querySelector(".toggle-button");
 
 function toggleNav() {
 	if (navbarLinks.classList.contains("active")) {
 		navbarLinks.classList.remove("active");
+		toggleButton.ariaExpanded = "false";
 	} else {
 		navbarLinks.classList.add("active");
+		toggleButton.ariaExpanded = "true";
 	}
 }
